e5cdd55e50058e50ddd909aa1b831810dd72928a
henrique <122335123+hevrique@users.noreply.github.com>
2024-09-12 22:07:06 -0300

    dados.js

---
+++ dados.js
@@ -0,0 +1,50 @@
+let artistas = [
+    {
+        título: "Charli XCX",
+        descrição: "Charli XCX, uma sereia sintetizada, navega em mares de pixels e batidas trituradas, pintando paisagens sonoras vibrantes e distorcidas. Sua voz, um laser neon cortando a escuridão, conduz hinos pop futuristas, onde a melodia é um vírus contagioso e a letra, um manifesto de uma geração digital em constante mutação. Em um universo onde a realidade se esvai e a fantasia se torna tangível, Charli é a rainha indiscutível do PC Music, reinando sobre um império de corpos digitais e almas sintetizadas.",
+        link: "https://www.charlixcx.com/",
+        imagem: "https://pbs.twimg.com/media/GJ2POxfXsAAaYae.jpg"
+    },
+    {
+        título: "Caroline Polachek",
+        descrição: "Caroline Polachek, uma feiticeira pós-moderna, desliza entre os gêneros como um fantasma, tecendo tapetes sonoros opulentos e hipnóticos. Sua voz, uma escultura de cristal, flutua em paisagens sonoras exuberantes, repletas de texturas e camadas infinitas. Num futuro distópico onde a música se torna um produto homogêneo, Caroline surge como uma visionária, reconfigurando paisagens sonoras que desafiam a compreensão. Criando obras de arte que transcendem o tempo e o espaço.",
+        link: "https://www.carolinepolachek.com/",
+        imagem: "https://jeff.4zzz.org.au/sites/default/files/review/unnamed%288%29.jpg"
+    },
+    {
+        título: "Yaeji",
+        descrição: "Yaeji, uma alquimista cósmica, transmuta o caos em ouro, criando mantras eletrônicos que evocam os espíritos ancestrais. Sua voz, um labirinto de sussurros e gritos, nos conduz por um mundo de contrastes, onde a dor se transforma em êxtase e a solidão se torna uma comunhão com o cosmos. Em um mundo fragmentado, ela nos oferece um elixir espiritual, capaz de curar as feridas mais profundas da alma e nos conectar com a força vital do universo.",
+        link: "https://www.yaeji.com/",
+        imagem: "https://lastfm.freetls.fastly.net/i/u/770x0/cc7da26bee0f07b7e6b466ccd7f416aa.jpg#cc7da26bee0f07b7e6b466ccd7f416aa"
+    },
+    {
+        título: "Grimes",
+        descrição: "Grimes, uma cyberpunk rebelde, evoca sinfonias cósmicas que desafiam os limites da percepção humana. Sua voz, um pulsar de estrelas, nos isola em um universo paralelo onde a carne e o metal se entrelaçam em uma dança frenética. Em um mundo cada vez mais dominado pela tecnologia, Grimes celebra a fusão entre o orgânico e o artificial, criando um algoritmo futurista onde a inteligência artificial não é apenas uma ferramenta, mas uma musa inspiradora.",
+        link: "https://grimesmusic.com/",
+        imagem: "https://i.pinimg.com/736x/d1/f7/a0/d1f7a0fc4acc0abb00656ccf6832f726.jpg"
+    },
+    {
+        título: "Tove Lo",
+        descrição: "Tove Lo, uma sacerdotisa da noite, habita um reino de sombras e melancolia, onde a dor se transforma em arte. Suas canções, baladas góticas e hipnóticas, nos convidam a uma jornada introspectiva pelas profundezas da alma humana. Em sua música, encontramos um refúgio seguro para explorar as profundezas da nossa psique e celebrar a beleza da imperfeição.",
+        link: "https://www.tovelo.com/",
+        imagem: "https://lastfm.freetls.fastly.net/i/u/ar0/61b8ce16f82aaff5932aa396609cc7f0.jpg"
+    },
+    {
+        título: "Eartheater",
+        descrição: "Eartheater, uma ninfa digital, transmuta o caos em catarses cósmicas, forjando paisagens sonoras que são tanto abismos quanto catedrais. Sua voz, um ciclone de emoções cruas e distorções, nos arrasta para um vórtice de sensações, onde a realidade se liquefaz e se recombina em formas estranhas e maravilhosas. Suas letras nos convidam a uma dança frenética nos limites da percepção, celebrando a beleza insana da transformação.",
+        link: "https://www.eartheater.com/",
+        imagem: "https://p-a-n.org/wp-content/uploads/2020/08/website.jpg"
+    },
+    {
+        título: "Magdalena Bay",
+        descrição: "Magdalena Bay, uma estrela errante em um universo sonoro, tece melodias que ecoam dos confins da galáxia. Sua voz, um cometa luminoso, percorre constelações de sintetizadores, criando paisagens sonoras que nos arrebatam para dimensões paralelas. Em cada nota, um portal se abre, revelando um labirinto de emoções, onde a nostalgia se encontra com o futuro, e a melancolia dança com a euforia.",
+        link: "https://imaginaldisk.world/",
+        imagem: "https://api.floodmagazine.com/wp-content/uploads/2024/08/Magdalena-Bay-Imaginal-Disk.jpg"
+    },
+    {
+        título: "Slayyyter",
+        descrição: "Em um universo de pixels e batidas frenéticas, onde o sintetizador reina supremo e a nostalgia dos anos 80 se funde com a estética cyberpunk, surge Slayyyter, uma deusa neon que desafia os limites da música pop. Sua voz, uma sirene hipnotizante, nos conduz por paisagens sonoras exuberantes, repletas de melodias cativantes e letras provocantes. Slayyyter é a alquimia perfeita entre o caos e a ordem, o sublime e o vulgar. ",
+        link: "https://slayyyter.com/",
+        imagem: "https://i1.sndcdn.com/artworks-6W16YIa8xLYI-0-t1080x1080.jpg"
+    }
+];
